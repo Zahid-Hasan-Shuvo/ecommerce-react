ad3c29e7baa69f0395ea6f48ba87f75d2549b9f7
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-01-27 16:10:24 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+     <nav className='header'>
+      <img src={logo} alt="" />
+<div>
+  <a href="/home">Home</a>
+  <a href="/order">Order</a>
+   <a href="/inventory">inventory</a>
+  <a href="/about">About</a>
+</div>
+
+     </nav>
+    </div>
+  );
+};
+
+export default Header;
