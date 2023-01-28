5b3d08f7f3db94d02fa940a020ae4f17a2e6a246
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-01-28 16:29:24 +0600

    card-dynamic

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
 const {name, img, seller, price, ratings} =props.product;
+
   return (
     <div className='product'>
    
@@ -13,8 +16,9 @@ const {name, img, seller, price, ratings} =props.product;
       <p><small>Seller: {seller}</small></p>
       <p><small>Ratings: {ratings} stars </small></p>
      </div>
-     <button className='btn-cart'>
-      <p>Add to cart</p>
+     <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'> 
+      <p className='cart-text'>Add to cart</p>
+      <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
      </button>
     </div>
   );
